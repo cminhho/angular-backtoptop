495030791757c07a1bc07492b43cd30177774af2
cminhho <hmchung92@gmail.com>
2015-07-06 17:18:17 +0700

    first commit

---
+++ src/angular-backtotop.js
@@ -0,0 +1,40 @@
+/**
+ * Created by hmchung2 on 7/6/15.
+ */
+(function () {
+  'use strict';
+  /**
+   * @ngdoc directive
+   * @name directive.backToTop:backToTop
+   * @scope
+   * @restrict E,A
+   *
+   * @description
+   * This "Back to top" directive link allows users to smoothly scroll back to the top of the page
+   *
+   */
+  angular
+    .module('directive.backToTop', [])
+    .directive('backToTop', backToTop);
+
+  /* @ngInject */
+  function backToTop() {
+    // Usage: <div back-to-top/>
+    // 
+    // Creates: hmchung2 on 7/6/15.
+    // 
+    var directive = {
+      link: link,
+      restrict: 'EA'
+    };
+    return directive;
+
+    function link(scope, element, attrs) {
+      element.on('click', function (evt) {
+        angular.element('body,html').animate({
+          scrollTop: 0
+        }, 800);
+      });
+    }
+  }
+})();
